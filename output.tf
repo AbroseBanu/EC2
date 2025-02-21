03ce42f1da41eff345aad9b8a62839a262c78770
AbroseBanu <133941310+AbroseBanu@users.noreply.github.com>
2025-02-21 10:45:16 +0530

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,9 @@
+output "instance_public_ips" {
+  description = "Public IP addresses of the EC2 instances"
+  value       = aws_instance.example[*].public_ip
+}
+
+output "instance_private_ips" {
+  description = "Private IP addresses of the EC2 instances"
+  value       = aws_instance.example[*].private_ip
+}
